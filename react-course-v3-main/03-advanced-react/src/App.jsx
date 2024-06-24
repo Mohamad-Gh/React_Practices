171bcf5622ea10bbd763ba3fceedff8f7af2c60a
unknown <mohamad.ghalebizade@gmail.com>
2024-06-23 19:11:24 -0700

    my solution for 02-lower-state-challenge

--- react-course-v3-main/03-advanced-react/src/App.jsx
+++ react-course-v3-main/03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from "./tutorial/11-performance/starter/01-lower-state";
-import Final from "./tutorial/11-performance/final/01-lower-state";
+import Starter from "./tutorial/11-performance/starter/02-lower-state-challenge";
+import Final from "./tutorial/11-performance/final/02-lower-state-challenge";
 function App() {
   return (
     <div className="container">
