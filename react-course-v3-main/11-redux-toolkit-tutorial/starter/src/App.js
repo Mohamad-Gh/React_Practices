74bbf44a57769430df53b4ca5c84cf45635f68e3
unknown <mohamad.ghalebizade@gmail.com>
2024-07-02 23:32:43 -0700

    initiate team app

--- react-course-v3-main/11-redux-toolkit-tutorial/starter/src/App.js
+++ react-course-v3-main/11-redux-toolkit-tutorial/starter/src/App.js
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+import CartContainer from "./components/CartContainer";
+import NavBar from "./components/NavBar";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+  return (
+    <main>
+      <NavBar />
+      <CartContainer />
+    </main>
+  );
 }
 export default App;
---
+++ react-teams/my-team/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import Employees from "./Employees";
+
+function App() {
+  return (
+    <>
+      {/* <h1>Hello, World</h1> */}
+      <Employees />
+    </>
+  );
+}
+
+export default App;
