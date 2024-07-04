1c84d40cc488663b91599198e1f99ac891c0863e
unknown <mohamad.ghalebizade@gmail.com>
2024-07-04 00:18:35 -0700

    add localStorage function with useEffect for employeeList and teams

--- react-teams/my-team/src/App.jsx
+++ react-teams/my-team/src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Employees from "./Employees";
 import teamMembers from "./Data";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
-  const [selectedTeam, setTeam] = useState("TeamB");
-  const [employees, setEmployees] = useState(teamMembers);
+  const [selectedTeam, setTeam] = useState(
+    JSON.parse(localStorage.getItem("selectedTeam")) || "TeamB"
+  );
+  const [employees, setEmployees] = useState(
+    JSON.parse(localStorage.getItem("employeeList")) || teamMembers
+  );
   const handleEmployeeCardClick = (event) => {
     const transformedEmployee = employees.map((employee) =>
       employee.id === parseInt(event.currentTarget.id)
@@ -20,6 +25,13 @@ function App() {
   function handleTeamChange(event) {
     setTeam(event.target.value);
   }
+
+  useEffect(() => {
+    localStorage.setItem("employeeList", JSON.stringify(employees));
+  }, [employees]);
+  useEffect(() => {
+    localStorage.setItem("selectedTeam", JSON.stringify(selectedTeam));
+  }, [selectedTeam]);
   return (
     <>
       <Header selectedTeam={selectedTeam} employees={employees} />
@@ -29,6 +41,7 @@ function App() {
         handleEmployeeCardClick={handleEmployeeCardClick}
         handleTeamChange={handleTeamChange}
       />
+      <Footer />
     </>
   );
 }
