32ceb654253a232b0065722b0fcbb9c01543b8dd
unknown <mohamad.ghalebizade@gmail.com>
2024-07-03 19:03:30 -0700

    add 2 images and use gender to show male or female img for each card

--- react-teams/my-team/src/App.jsx
+++ react-teams/my-team/src/App.jsx
@@ -5,7 +5,6 @@ import Employees from "./Employees";
 function App() {
   return (
     <>
-      {/* <h1>Hello, World</h1> */}
       <Employees />
     </>
   );
--- react-teams/my-team/src/Employees.jsx
+++ react-teams/my-team/src/Employees.jsx
@@ -1,90 +1,7 @@
 import React, { useState } from "react";
-const teamMembers = [
-  {
-    id: 1,
-    fullName: "Bob Jones",
-    designation: "JavaScript Developer",
-    gender: "male",
-    teamName: "TeamA",
-  },
-  {
-    id: 2,
-    fullName: "Jill Bailey",
-    designation: "Node Developer",
-    gender: "female",
-    teamName: "TeamA",
-  },
-  {
-    id: 3,
-    fullName: "Gail Shepherd",
-    designation: "Java Developer",
-    gender: "female",
-    teamName: "TeamA",
-  },
-  {
-    id: 4,
-    fullName: "Sam Reynolds",
-    designation: "React Developer",
-    gender: "male",
-    teamName: "TeamB",
-  },
-  {
-    id: 5,
-    fullName: "David Henry",
-    designation: "DotNet Developer",
-    gender: "male",
-    teamName: "TeamB",
-  },
-  {
-    id: 6,
-    fullName: "Sarah Blake",
-    designation: "SQL Server DBA",
-    gender: "female",
-    teamName: "TeamB",
-  },
-  {
-    id: 7,
-    fullName: "James Bennet",
-    designation: "Angular Developer",
-    gender: "male",
-    teamName: "TeamC",
-  },
-  {
-    id: 8,
-    fullName: "Jessica Faye",
-    designation: "API Developer",
-    gender: "female",
-    teamName: "TeamC",
-  },
-  {
-    id: 9,
-    fullName: "Lita Stone",
-    designation: "C++ Developer",
-    gender: "female",
-    teamName: "TeamC",
-  },
-  {
-    id: 10,
-    fullName: "Daniel Young",
-    designation: "Python Developer",
-    gender: "male",
-    teamName: "TeamD",
-  },
-  {
-    id: 11,
-    fullName: "Adrian Jacobs",
-    designation: "Vue Developer",
-    gender: "male",
-    teamName: "TeamD",
-  },
-  {
-    id: 12,
-    fullName: "Devin Monroe",
-    designation: "Graphic Designer",
-    gender: "male",
-    teamName: "TeamD",
-  },
-];
+import teamMembers from "./Data";
+import femaleProfile from "./images/femaleProfile.jpg";
+import maleProfile from "./images/maleProfile.jpg";
 
 function Employees() {
   const [employees, setEmployees] = useState(teamMembers);
@@ -92,8 +9,12 @@ function Employees() {
     <main>
       {employees.map((employee) => (
         <div key={employee.id}>
+          {employee.gender === "male" ? (
+            <img src={maleProfile} />
+          ) : employee.gender === "female" ? (
+            <img src={femaleProfile} />
+          ) : null}
           <p>{employee.fullName}</p>
-          <p>{employee.gender}</p>
         </div>
       ))}
     </main>
