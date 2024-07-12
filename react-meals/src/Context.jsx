716c903f2a0652a191f4fe74954b74080410761b
unknown <mohamad.ghalebizade@gmail.com>
2024-07-12 06:44:34 -0700

    fetch data

--- react-meals/src/Context.jsx
+++ react-meals/src/Context.jsx
@@ -1,12 +1,25 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect } from "react";
 const AppContext = createContext();
 
+const fetchData = async () => {
+  try {
+    const response = await fetch("https://randomuser.me/api/");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 function AppProvider({ children }) {
+  useEffect(() => {
+    fetchData();
+  }, []);
   return <AppContext.Provider value={"hello"}>{children}</AppContext.Provider>;
 }
-
-export { AppProvider, AppContext };
 
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
+
+export { AppProvider, AppContext };
