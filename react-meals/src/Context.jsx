86d3b2416736e3fdc86aa9a94c4907e470d8444a
unknown <mohamad.ghalebizade@gmail.com>
2024-07-12 22:10:48 -0700

    init using axios

--- react-meals/src/Context.jsx
+++ react-meals/src/Context.jsx
@@ -1,19 +1,38 @@
 import React, { createContext, useContext, useEffect } from "react";
+import axios from "axios";
+
+const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
 const AppContext = createContext();
 
-const fetchData = async () => {
+// const fetchData = async () => {
+//   try {
+//     const response = await fetch(
+//       "https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
+//     );
+//     const allMealsUrl = await response.json();
+//     console.log("search", allMealsUrl.meals[0]);
+//     const randomResponse = await fetch(
+//       "https://www.themealdb.com/api/json/v1/1/random.php"
+//     );
+//     const randomMealUrl = await randomResponse.json();
+//     console.log("random", randomMealUrl.meals[0]);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+const fetchMeals = async (url) => {
   try {
-    const response = await fetch("https://randomuser.me/api/");
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
+    const response = await axios(url);
+    console.log(response);
+  } catch (e) {
+    console.log(e);
   }
 };
-
 function AppProvider({ children }) {
   useEffect(() => {
-    fetchData();
+    fetchMeals(allMealsUrl);
   }, []);
   return <AppContext.Provider value={"hello"}>{children}</AppContext.Provider>;
 }
