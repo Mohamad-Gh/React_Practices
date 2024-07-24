ac12950ba364ace66f60328eb76453082b05cd24
unknown <mohamad.ghalebizade@gmail.com>
2024-07-23 22:20:10 -0700

    fetch data and set to stock

--- react-trend/react-trend-app/src/components/StockList.jsx
+++ react-trend/react-trend-app/src/components/StockList.jsx
@@ -3,28 +3,39 @@ import finnHub from "../Apis/Axios";
 function StockList() {
   const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState(null);
+  const [stock, setStock] = useState(null);
 
   useEffect(() => {
+    let isMounted = true;
     const fetchData = async () => {
       try {
-        const response = await finnHub.get("/quote", {
-          params: { symbol: "AAPL" },
+        const responses = await Promise.all(
+          watchList.map((stock) => {
+            return finnHub.get("/quote", { params: { symbol: stock } });
+          })
+        );
+        const data = responses.map((response) => {
+          return { data: response.data, symbol: response.config.params.symbol };
         });
-        console.log(response.data);
-
-        // setData(response.data);
+        if (isMounted) {
+          setStock(data);
+        }
+        console.log("data:", data);
+        setLoading(false);
       } catch (err) {
         console.log(err);
       }
     };
     fetchData();
+    return () => {
+      isMounted = false;
+    };
   }, []);
   if (loading) {
     return <div>Loading ...</div>;
   }
 
-  return <div>StockList</div>;
+  return <div>ok</div>;
 }
 
 export default StockList;
