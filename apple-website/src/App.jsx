51e1f1fb1d3b9c6d241fa53e562cbbd585c328a4
unknown <mohamad.ghalebizade@gmail.com>
2024-08-06 21:25:43 -0700

    init Navbar, Hero, and Highlights components

--- apple-website/src/App.jsx
+++ apple-website/src/App.jsx
@@ -1,5 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Highlights from "./components/Highlights/Highlights";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline ">Hello world!</h1>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 }
 
 export default App;
