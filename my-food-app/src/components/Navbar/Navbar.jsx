6f8202f047844b1ed5bf124c4c310fd80c09a2ea
unknown <mohamad.ghalebizade@gmail.com>
2024-08-21 11:26:57 -0700

    init my food app with tailwind

---
+++ my-food-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 ">
+        <div className="container">
+          <div className="flex"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
