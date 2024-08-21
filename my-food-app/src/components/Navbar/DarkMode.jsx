a2f4783a48d3aac109538d81078a741cd1e376d2
unknown <mohamad.ghalebizade@gmail.com>
2024-08-21 13:41:04 -0700

    setup darkMode function

--- my-food-app/src/components/Navbar/DarkMode.jsx
+++ my-food-app/src/components/Navbar/DarkMode.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import lightPng from "../../assets/website/light-mode-button.png";
 import darkPng from "../../assets/website/dark-mode-button.png";
 
@@ -6,11 +6,23 @@ function DarkMode() {
   const [darkModeTheme, setDarkModeTheme] = useState(
     localStorage.getItem("darkModeTheme") || false
   );
+
+  useEffect(() => {
+    const element = document.documentElement;
+    if (darkModeTheme) {
+      element.classList.add("dark");
+    } else {
+      element.classList.remove("dark");
+    }
+    localStorage.setItem("darkModeTheme", { darkModeTheme });
+  }, [darkModeTheme]);
+
+  const changeTheme = () => {
+    setDarkModeTheme(!darkModeTheme);
+  };
+
   return (
-    <button
-      className="flex items-center"
-      onClick={() => setDarkModeTheme(!darkMode)}
-    >
+    <button className="flex items-center" onClick={changeTheme}>
       <img
         className="w-12 cursor-pointer drop-shadow-[1px_1px_2px_rgba(0,0,0,0.5)] duration-300"
         src={!darkModeTheme ? lightPng : darkPng}
