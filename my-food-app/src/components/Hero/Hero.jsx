fd49eceb1c48c50b981f792fff1b9de3b0aa446d
unknown <mohamad.ghalebizade@gmail.com>
2024-08-21 18:49:02 -0700

    add rotation animation to image in hero component

--- my-food-app/src/components/Hero/Hero.jsx
+++ my-food-app/src/components/Hero/Hero.jsx
@@ -39,22 +39,35 @@ function Hero() {
         <div className="container pb-8 sm:pb-0">
           <div className="grid grid-cols-1 sm:grid-cols-2">
             <div className="flex flex-col gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1">
-              <h1 className="">
+              <h1 className="text-5xl font-bold sm:text-6xl lg:text-7xl">
                 Welcome to the <span className="text-primary">Foodie</span> Zone
               </h1>
+              <p className="text-sm">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit
+                nesciunt autem nemo rem magni sapiente beatae. Sed magnam cumque
+                odit?
+              </p>
               <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Vestibulum faucibus sagittis lacus, et sagittis turpis
-                pellentesque vel. Curabitur odio quam, tempus eget metus ac,
-                venenatis pulvinar elit.
-              </div>
-              <div>
-                <button className="bg-primary text-white text-xl rounded-full">
+                <button className="bg-gradient-to-r from-primary to-secondary text-white px-4 py-2 text-xl rounded-full hover:scale-105 duration-200">
                   Order Now
                 </button>
               </div>
             </div>
-            <div>Rotating Food Picture</div>
+            {/* image section */}
+            <div className="order-1 sm:order-2 min-h-[450px] sm:min-h-[450px] flex justify-center items-center relative">
+              {/* main image  */}
+              <div>
+                <img
+                  className="w-[300px] sm:w-[450px] mx-auto rounded-full spin"
+                  src={BiryaniImg3}
+                  alt="rotating-dish"
+                />
+              </div>
+              {/* image list */}
+              <div>
+                <img src="" alt="" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
