3668b1f80f140707477f0cd62fe94753a244634e
unknown <mohamad.ghalebizade@gmail.com>
2024-08-22 18:33:29 -0700

    init AppStore component

--- my-food-app/src/App.jsx
+++ my-food-app/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/ServicesComponent/Services";
 import Banner from "./components/Banner/Banner";
+import AppStore from "./components/AppStore/AppStore";
 export default function App() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function App() {
         <Hero />
         <Services />
         <Banner />
+        <AppStore />
       </div>
     </>
   );
---
+++ my-food-app/src/components/AppStore/AppStore.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppStore() {
+  return <div>AppStore</div>;
+}
+
+export default AppStore;
