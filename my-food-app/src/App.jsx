432b9802ac88ec24527a4f80b6ee5e6bfbe1eb2c
unknown <mohamad.ghalebizade@gmail.com>
2024-08-22 18:16:46 -0700

    services component done

--- my-food-app/src/App.jsx
+++ my-food-app/src/App.jsx
@@ -5,9 +5,11 @@ import Services from "./components/ServicesComponent/Services";
 export default function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Services />
+      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <Navbar />
+        <Hero />
+        <Services />
+      </div>
     </>
   );
 }
--- my-food-app/src/components/ServicesComponent/Services.jsx
+++ my-food-app/src/components/ServicesComponent/Services.jsx
@@ -45,13 +45,24 @@ function Services() {
             </p>
           </div>
           {/* Card Section */}
-          <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-14 md:grid-cols-3 md:gap-5 place-items-center">
             {ServicesData.map(({ id, img, name, description }) => (
-              <div>
-                <img key={id} src={img} alt={name} />
-                <div>
-                  <h2>{name}</h2>
-                  <p>{description}</p>
+              <div
+                key={id}
+                className="max-w-[300px] group rounded-2xl bg-white dark:bg-gray-800 dark:hover:bg-primary hover:bg-primary hover:text-white duration-300 shadow-xl"
+              >
+                <div className="h-[100px]">
+                  <img
+                    className="max-w-[200px] mx-auto block transform -translate-y-14 group-hover:scale-105 group-hover:rotate-6 duration-300"
+                    src={img}
+                    alt={name}
+                  />
+                </div>
+                <div className="p-4 text-center">
+                  <h1 className="text-xl font-bold">{name}</h1>
+                  <p className="text-gray-500 text-sm line-clamp-2 group-hover:text-white duration-300">
+                    {description}
+                  </p>
                 </div>
               </div>
             ))}
