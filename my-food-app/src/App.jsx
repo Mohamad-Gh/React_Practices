6709708a91a89d1dfd11ffc77913f4fa8d4d70dc
unknown <mohamad.ghalebizade@gmail.com>
2024-08-22 18:17:34 -0700

    init banner component

--- my-food-app/src/App.jsx
+++ my-food-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/ServicesComponent/Services";
+import Banner from "./components/Banner/Banner";
 export default function App() {
   return (
     <>
@@ -9,6 +10,7 @@ export default function App() {
         <Navbar />
         <Hero />
         <Services />
+        <Banner />
       </div>
     </>
   );
